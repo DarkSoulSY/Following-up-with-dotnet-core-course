214236e77e7c2753ba08c36c0df72ac5f294f362
Nasser Abdullah <doctorgtnasser@hotmail.com>
2022-11-17 10:21:27 +0200

    Added A DTO instead of using model Villa itself, also Updated the Controller

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,6 +1,22 @@
-﻿namespace MagicVilla_VillaAPI.Controllers
+﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
 {
-    public class VillaAPIController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController: ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return new List<VillaDTO>
+            {
+                new VillaDTO {Id=1,Name="Pool View"},
+                new VillaDTO {Id=2,Name="Beach View"}
+            };
+        }
+
     }
 }
